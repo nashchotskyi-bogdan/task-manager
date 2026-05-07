8609fd764c36de4c5bc3f646aeda974f77e9c15f
Bogdan <naszczotskyib@gmail.com>
2026-05-07 17:27:31 +0200

    changes_4

--- backend/settings.py
+++ backend/settings.py
@@ -81,7 +81,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "frontend"]
+STATICFILES_DIRS = [BASE_DIR / "backend" / "frontend"]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
