aaff8f1e55b3035e56d070e854d18c5c9c6d2f2a
Bogdan <naszczotskyib@gmail.com>
2026-05-09 22:49:50 +0200

    changes_11

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet, ProjectViewSet, CategoryViewSet, CommentViewSet
 router = DefaultRouter()
-router.register(r'tasks', TaskViewSet, basename='tasks')
-router.register(r'projects', ProjectViewSet, basename='projects')
-router.register(r'categories', CategoryViewSet, basename='categories')
-router.register(r'comments', CommentViewSet, basename='comments')
+router.register(r"tasks", TaskViewSet, basename="tasks")
+router.register(r"projects", ProjectViewSet, basename="projects")
+router.register(r"categories", CategoryViewSet, basename="categories")
+router.register(r"comments", CommentViewSet, basename="comments")
 urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,18 +1,15 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Task, Project, Category, Comment
-from .serializers import (
-    TaskSerializer,
-    ProjectSerializer,
-    CategorySerializer,
-    CommentSerializer
-)
+from .serializers import TaskSerializer, ProjectSerializer, CategorySerializer, CommentSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
     def get_queryset(self):
         return Task.objects.filter(project__owner=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save()
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
@@ -23,12 +20,17 @@ class ProjectViewSet(viewsets.ModelViewSet):
         serializer.save(owner=self.request.user)
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        return Category.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
     def get_queryset(self):
         return Comment.objects.filter(task__project__owner=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save()
