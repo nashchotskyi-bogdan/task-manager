5d62d64aa77549c4bcdefcf157a9542b7918499a
Bogdan <naszczotskyib@gmail.com>
2026-05-10 10:15:33 +0200

    changes_17

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,17 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, ProjectViewSet, CategoryViewSet, CommentViewSet
+from .views import (
+    TaskViewSet,
+    ProjectViewSet,
+    CategoryViewSet,
+    CommentViewSet,
+    register
+)
 router = DefaultRouter()
 router.register(r"tasks", TaskViewSet, basename="tasks")
 router.register(r"projects", ProjectViewSet, basename="projects")
 router.register(r"categories", CategoryViewSet, basename="categories")
 router.register(r"comments", CommentViewSet, basename="comments")
-urlpatterns = router.urls
-from django.urls import path
-from .views import register
-urlpatterns = [
-    path("register/", register)
+urlpatterns = router.urls + [
+    path("register/", register),
 ]
